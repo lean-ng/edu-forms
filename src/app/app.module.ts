082e1a64121d3febd6cd8548e77ff133cb3dfff8
Micha Buchholz <mail@lean-stack.de>
2023-07-04 21:56:09 +0200

    chore: Add navbar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppNavComponent } from './components/app-nav/app-nav.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AppNavComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/app-nav/app-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './app-nav.component.html',
+  styles: [
+  ]
+})
+export class AppNavComponent {
+
+}

547beb9b2f6775f1edb114912d4e240362dcc66e
Micha Buchholz <mail@lean-stack.de>
2023-07-05 08:42:44 +0200

    chore: Add product list view.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,16 @@ import { AppComponent } from './app.component';
 import { AppNavComponent } from './components/app-nav/app-nav.component';
 import { HomeComponent } from './views/home/home.component';
 import { PlainHtmlComponent } from './views/plain-html/plain-html.component';
+import { ProductListComponent } from './views/product-list/product-list.component';
 
 @NgModule({
-  declarations: [AppComponent, AppNavComponent, HomeComponent, PlainHtmlComponent],
+  declarations: [
+    AppComponent,
+    AppNavComponent,
+    HomeComponent,
+    PlainHtmlComponent,
+    ProductListComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
